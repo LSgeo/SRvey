3450a4d0781551032ab3ab4769d114bfe7b80704
LSgeo <LukeSmith.geo@gmail.com>
2022-06-22 15:29:36 +0800

    Add incorrectly ignored data submodule

---
+++ srvey/data/__init__.py
@@ -0,0 +1,223 @@
+print(f"Loading {__name__}")
+import logging
+
+import numpy as np
+import verde as vd
+import torch
+from torch.utils.data import DataLoader, Subset
+
+import srvey.cfg as cfg
+from mlnoddy.datasets import NoddyDataset
+
+
+def norm_noddy_tensor(batch):
+    norm = lambda i: i  # TODO
+    norm_data = norm(batch)
+    return norm_data
+
+
+def build_dataloaders():
+    """Returns dataloaders for Training, Validation, and image previews"""
+
+    train_dataset = HRLRNoddyDataset(
+        model_dir=cfg.train_tiles_path, **cfg.dataset_config
+    )
+    val_dataset = HRLRNoddyDataset(model_dir=cfg.val_tiles_path, **cfg.dataset_config)
+    preview_dataset = Subset(val_dataset, cfg.preview_indices)
+
+    logging.getLogger("data").info(f"{len(train_dataset)=}")
+    logging.getLogger("data").info(f"{len(val_dataset)=}")
+    logging.getLogger("data").info(f"{len(preview_dataset)=}")
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        pin_memory=cfg.pin_memory,
+        batch_size=cfg.trn_batch_size,
+        num_workers=cfg.num_workers,
+        shuffle=cfg.shuffle,
+        drop_last=False,
+    )
+    validation_dataloader = DataLoader(
+        val_dataset,
+        pin_memory=cfg.pin_memory,
+        batch_size=cfg.val_batch_size,
+        num_workers=cfg.num_workers,
+        persistent_workers=bool(cfg.num_workers),
+        shuffle=False,
+    )
+    preview_dataloader = DataLoader(
+        preview_dataset,
+        pin_memory=cfg.pin_memory,
+        batch_size=min(cfg.val_batch_size, len(preview_dataset)),
+    )
+
+    return train_dataloader, validation_dataloader, preview_dataloader
+
+
+def subsample(parameters: dict, scale=1, *rasters):
+    input_cell_size = 20
+    """Run a mock-survey on a geophysical raster.
+    Designed for use with Noddy forward models, as part of a Pytorch dataset.
+
+    Args:
+        parameters:
+            line_spacing: in meters, spacing between parallel lines
+            sample_spacing: in meters, spacing between points along line
+            heading: "NS" for columns as lines, "EW" for rows as lines
+        scale: multiplier for low resolution (vs high resolution)
+        input_cell_size: ground truth cell size, 20 m for Noddy models
+        *rasters: input Tensor forward model
+
+    The Noddyverse dataset is a suite of 1 Million 200x200x200 petrophysical
+    voxels, at a designated size of 20 m per pixel. Forward models in the
+    Noddyverse (https://doi.org/10.5194/essd-14-381-2022) are generated
+    as per below:
+        Geophysical forward models were calculated using a Fourier domain
+        formulation using reflective padding to minimise (but not remove)
+        boundary effects. The forward gravity and magnetic field calculations
+        assume a flat top surface with a 100 m sensor elevation above this
+        surface and the Earth's magnetic field with vertical inclination,
+        zero declination and an intensity of 50000nT.
+
+    Note that every single cell of the forward model has a calculated forward
+    model, i.e. they are 200x200, with no interpolation (for a 20 m cell size)
+
+    We simulate an airborne survey, by selecting rows (flight lines) of pixels
+    at every n m. We can (but not by default) also subsample along rows (ss).
+
+    """
+    cs = input_cell_size
+    ss = int(parameters.get("sample_spacing") / cs)
+    ls = int(parameters.get("line_spacing") * scale / cs)
+
+    if parameters.get("heading").upper() in ["EW", "E", "W"]:
+        ls, ss = ss, ls  # swap convention to emulate survey direction
+
+    x, y = np.meshgrid(np.arange(200), np.arange(200), indexing="xy")
+    x = cs * x[::ls, ::ss]
+    y = cs * y[::ls, ::ss]
+    zs = [raster[::ls, ::ss] for raster in rasters]
+
+    return x, y, zs
+
+
+def grid(x, y, zs, ls: int = 20, cs_fac: int = 4):
+    in_cs: int = 20
+    """Grid a subsampled noddy forward model.
+
+    params:
+        x, y: x, y coordinates
+        z: geophysical response value
+        line_spacing: sample line spacing, to calculate target cell_size
+        cs_fac: line spacing to cell size factor, typically 4 or 5
+        name: data_variable name
+        input_cell_size: Input model cell size, 20m for Noddyverse
+
+    See docstring for subsample() for further notes.
+    """
+    for rz in zs:
+        gridder = vd.ScipyGridder("cubic").fit((x, y), rz)
+
+        yield gridder.grid(
+            region=[0, in_cs * 200, 0, in_cs * 200],
+            spacing=ls / cs_fac,
+            dims=["x", "y"],
+            data_names="forward",
+        ).get("forward").values.astype(np.float32)
+
+
+def make_coord(shape, ranges=None, flatten=True):
+    """Make coordinates at grid centers.
+
+    https://github.com/jaewon-lee-b/lte/blob/main/utils.py#L104
+    """
+    coord_seqs = []
+    for i, n in enumerate(shape):
+        if ranges is None:
+            v0, v1 = -1, 1
+        else:
+            v0, v1 = ranges[i]
+        r = (v1 - v0) / (2 * n)
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
+        coord_seqs.append(seq)
+    ret = torch.stack(torch.meshgrid(*coord_seqs, indexing="ij"), dim=-1)
+    if flatten:
+        return ret.view(-1, ret.shape[-1])
+    else:
+        return ret
+
+
+def to_pixel_samples(grid):
+    """Convert the image to coord-*C pairs.
+    grid: Tensor, (C, H, W)
+    https://github.com/jaewon-lee-b/lte/blob/main/utils.py#L122
+
+    Returns:
+        coordinate array (shape = H*W, 2) and values in make_coord range
+        c_vals: H*W list of tuples of channels value(s) at coordinate n
+
+    i.e. return two lists describing the index and channel(s) value(s) at
+         index of input grid.
+
+
+    """
+    if len(grid.shape) != 3:  # C,H,W, data are not batched yet.
+        raise ValueError(f"Grid should be of shape C,H,W by now. Got {grid.shape}")
+    coord = make_coord(grid.shape[-2:])
+    c_vals = grid.view(grid.shape[1], -1).permute(1, 0)
+    return coord, c_vals
+
+
+class HRLRNoddyDataset(NoddyDataset):
+    def __init__(self, **kwargs):
+        self.scale = kwargs.get("scale", 2)
+        self.random_heading = not bool(kwargs.get("heading"))
+        self.sp = {
+            "line_spacing": kwargs.get("line_spacing", 20),
+            "sample_spacing": kwargs.get("sample_spacing", 20),
+            "heading": kwargs.get("heading", None),  # Default will be random
+        }
+        super().__init__(**kwargs)
+
+    def _process(self, index):
+        super()._process(index)
+
+        hls = self.sp["line_spacing"]
+        lls = hls * self.scale
+        if self.random_heading:
+            if torch.rand(1) < 0.5:
+                self.sp["heading"] = "NS"
+            else:
+                self.sp["heading"] = "EW"
+
+        hr_x, hr_y, _hr_zs = subsample(self.sp, 1, *self.data["gt"])
+        _hr_grids = [
+            torch.from_numpy(g) for g in grid(hr_x, hr_y, _hr_zs, ls=hls, cs_fac=4)
+        ]
+        self.data = {"hr": torch.stack(_hr_grids, dim=0), **self.data}
+
+        lr_x, lr_y, _lr_zs = subsample(self.sp, self.scale, *self.data["gt"])
+        _lr_grids = [
+            torch.from_numpy(g) for g in grid(lr_x, lr_y, _lr_zs, ls=lls, cs_fac=4)
+        ]
+        self.data = {"lr": torch.stack(_lr_grids, dim=0), **self.data}
+
+        self.data["coord"], self.data["c_vals"] = to_pixel_samples(
+            self.data["hr"].contiguous()
+        )
+
+        # TODO Understand what sample_q is doing from
+        # https://github.com/jaewon-lee-b/lte/blob/94bca2bf5777b76edbad46e899a1c2243e3751d4/datasets/wrappers.py#L64
+
+        self.data["cell"] = torch.ones_like(self.data["coord"])
+        self.data["cell"][:, 0] *= 2 / self.data["hr"].shape[-2]
+        self.data["cell"][:, 1] *= 2 / self.data["hr"].shape[-1]
+
+        # # self.data at this point may look like (some optional):
+        # {
+        #     "label": encode_label(self.parent),
+        #     "gt": torch.stack(gt_data, dim=0),
+        #     "hr": torch.stack(hr_grids, dim=0),
+        #     "lr": torch.stack(lr_grids, dim=0),
+        #     "geo": torch.from_numpy($2d_array_of_layer_n)),
+        # }
---
+++ srvey/data/statemap/datasets.py
@@ -0,0 +1,155 @@
+import logging
+from pathlib import Path
+
+
+import numpy as np
+import tifffile
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+
+import cfg
+from mlnoddy.datasets import HRLRNoddyDataset
+
+
+class BaseDataset(Dataset):
+    def __init__(self, tile_path, augment: bool = False):
+        super().__init__()
+
+        self.data = {}
+        self.augment = augment
+
+        self.tile_path = Path(tile_path)
+        self.hr_size = cfg.hr_size
+
+        # self._get_npy_data()
+        self._get_tif_data()
+
+        print(self._len)
+
+    def _get_npy_data(self, search="**/*.npy"):
+        for f in Path(self.tile_path).glob(search):
+            print(f.stem)
+            self.data[f"{f.stem}"] = np.load(f, mmap_mode="c").astype(np.float32)
+            if "hr" in f.stem.lower():
+                self.hr_key = f"{f.stem}"
+                self._len = len(self.data[f"{f.stem}"])
+            if "lr" in f.stem.lower():
+                self.lr_key = f"{f.stem}"
+        logging.getLogger("data").info(f"Found {self.data.keys()} in {self.tile_path}")
+
+    def _get_tif_data(self, search="**/*.tif"):
+        self.hr_key = "hr"
+        self.lr_key = "lr"
+        self.data[self.hr_key] = []
+        self.data[self.lr_key] = []
+
+        for f in Path(self.tile_path).glob(search):
+            self.data[f"{f.parts[-2].lower()}"].append(
+                tifffile.imread(f).astype(np.float32)
+            )
+
+        self._len = len(self.data[self.hr_key])
+        logging.getLogger("data").info(
+            f"Found {self._len} files in {self.data.keys()} in {self.tile_path}"
+        )
+
+    def _min_max_norm(self, tile):
+        return (tile - self.min) / (self.max - self.min)
+
+    def __len__(self):
+        return self._len
+
+    def __getitem__(self, index):
+        tile_hr = self.data[self.hr_key][index].astype(np.float32)
+        tile_lr = self.data[self.lr_key][index].astype(np.float32)
+        tile_hr = torch.as_tensor(tile_hr, dtype=torch.float32).unsqueeze(0)
+        tile_lr = torch.as_tensor(tile_lr, dtype=torch.float32).unsqueeze(0)
+
+        if self.augment:
+            if torch.rand(1) < 0.5:
+                tile_hr = tile_hr.flip(-1)  # hflip
+                tile_lr = tile_lr.flip(-1)
+            if torch.rand(1) < 0.5:
+                tile_hr = tile_hr.flip(-2)  # vflip
+                tile_lr = tile_lr.flip(-2)
+            if torch.rand(1) < 0.5:
+                tile_hr = tile_hr.rot90(1, [-2, -1])  # rotate CHW Tensor CCW
+                tile_lr = tile_lr.rot90(1, [-2, -1])  # Tensor is not batched yet
+
+        return {"hr": tile_hr, "lr": tile_lr}
+
+
+class MyDset(BaseDataset):
+    """Quick tiff version of base dataset for ESRGAN RDN test with normalised data"""
+
+    def __init__(self, tile_path, augment: bool = False):
+        self.hr_key = "hr"
+        self.scales = []
+        self.set_scale((4, 4))
+
+        super().__init__(tile_path, augment)
+
+    def set_scale(self, scale: tuple):
+        """Set scale factor to load next iteration"""
+        if scale[0] != scale[1]:
+            raise NotImplementedError
+        self.curr_scale = scale[0]
+        self.lr_key = "lr"
+
+
+class ArbsrDset(BaseDataset):
+    """Read multiple LR and HR data with different geophysical scales.
+
+    These data are float32 single channel, and their resolution are fixed
+    at n times different to "hr". 1 / [2, 2.5,  3,  4,  5]
+    For tiles, this means HR 240, LR [120, 96, 80, 60, 48] pixels in each dim.
+    Train, Val, and Test data are organised into folders appended by "x.y",
+    indicating their decimal scale factor.
+
+    We include all available file paths for each optioned scale in a dictionary,
+    load the memmapped numpy arrays into another dictionary, and access them
+    during training by specifying the desired scale in a method. It is important
+    to ensure the values of each dict are sorted so matching indices refers to
+    data from the same extent.
+
+    """
+
+    def __init__(self, tile_path, augment: bool = False):
+        self.hr_key = "1.0"
+        self.scales = []
+        self.set_scale((4, 4))
+
+        super().__init__(tile_path, augment)
+
+        # Old WA Tile Dataset
+        # self.max = 14051.333
+        # self.min = -4403.574
+        # self.mean = -26.282
+        # self.std = 224.668
+
+        # # P738 LR training tiles, rounded to larger magnitude
+        # self.max = 213
+        # self.min = -315
+        # self.mean = -38
+        # self.std = 40
+
+    def set_scale(self, scale: tuple):
+        """Set scale factor to load next iteration"""
+        if scale[0] != scale[1]:
+            raise NotImplementedError
+        self.curr_scale = scale[0]
+        self.lr_key = f"{self.curr_scale:.1f}"
+
+    def _get_npy_data(self, search="*.npy"):
+        for f in Path(self.tile_path).glob(search):
+            scale = f"{f.stem.split('_')[-1].replace('-', '.')}"
+            self.data[scale] = np.load(f, mmap_mode="c").astype(np.float32)
+            self.scales.append(scale)
+
+        self._len = len(self.data["1.0"])
+
+        if not self.tile_path.exists():
+            raise FileNotFoundError(f"{self.tile_path} does not exist!")
+        logging.getLogger("data").info(f"Found {self.data.keys()} in {self.tile_path}")
+        if not self.data.get("1.0").any():
+            raise FileNotFoundError(f"Could not load HR data from {self.tile_path}")
